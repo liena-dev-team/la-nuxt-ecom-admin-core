ba964f7ec307c9a386d53230f2e9047bf86777e3
HQ-Binh <binh.hq@liena.v>
2025-12-26 04:52:38 +0000

    change npmignore add node module

--- app/composables/useApiFetch.js
+++ app/composables/useApiFetch.js
@@ -1,3 +1,4 @@
+import { useRequestHeaders } from '#app';
 
 export const useApiFetch = async (url, opts) => {
 	const headers = useRequestHeaders(['cookie'])
@@ -17,7 +18,7 @@ export const useApiFetch = async (url, opts) => {
 					}]
 				}
 			}
-	
+
 			console.log("Fetch Catch Error:");
 			console.log(error);
 		});
@@ -27,7 +28,7 @@ export const useApiFetch = async (url, opts) => {
 		console.log("Fetch Error:");
 		console.log(e);
 	}
-	
+
 	return null;
-	
+
 };
