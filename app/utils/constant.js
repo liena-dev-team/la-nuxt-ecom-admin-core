e36f9ee9a48a3cc4ec63cb09d5a78519050c63ba
HQ-Binh <binh.hq@liena.v>
2025-12-26 08:26:27 +0000

    test

---
+++ app/utils/constant.js
@@ -0,0 +1,14 @@
+
+
+// /**
+//  * FIELD TREE
+//  */
+export const FIELD_TREE = {
+	SELECT_TYPE_NONE: "none",
+	SELECT_TYPE_LEAF: "leaf",
+	SELECT_TYPE_SINGLE_LEAF: "single-leaf",
+	SELECT_TYPE_INDEPENDENT: "independent",
+	SELECT_TYPE_SINGLE_INDEPENDENT: "single-independent",
+	SELECT_TYPE_CLASSIC: "classic",
+}
+
